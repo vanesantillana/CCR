6f074498e1a8f0ad5333ef76424314f76218f712
Nash <anibal.ventura@ucsp.edu.pe>
2018-04-18 21:15:43 -0500

    juego funcionadno

--- game/server2.cpp
+++ game/server2.cpp
@@ -1,3 +1,8 @@
+#include <ncurses.h>        //g++ -o g.exe g.cpp  -lncurses  -std=c++11  q -lpthread
+#include <iostream>       // std::cout
+#include <thread>         // std::thread, std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,6 +19,66 @@
 using  namespace std; 
 StringMap Users;
 thread t[1000];
+typedef struct _win_border_struct {
+  chtype  ls, rs, ts, bs,
+    tl, tr, bl, br;
+}WIN_BORDER;
+typedef struct _WIN_struct {
+  int startx, starty;
+  int height, width;
+  WIN_BORDER border;
+}WIN;
+void init_win_params(WIN *p_win);
+void print_win_params(WIN *p_win){}
+void create_box(WIN *win, bool flag);
+//void bullet(WIN *p_win){}
+void jugar(WIN win){
+  
+}
+
+
+
+
+void init_win_params(WIN *p_win){
+  p_win->height = 8;
+  p_win->width = 16;
+  p_win->starty = (LINES - p_win->height)/2;
+  p_win->startx = (COLS - p_win->width)/2;
+  p_win->border.ls = '|';
+  p_win->border.rs = '|';
+  p_win->border.ts = '-';
+  p_win->border.bs = '-';
+  p_win->border.tl = '+';
+  p_win->border.tr = '+';
+  p_win->border.bl = '+';
+  p_win->border.br = '+';
+}
+void create_box(WIN *p_win, bool flag){
+  int i, j;
+  int x, y, w, h;
+  x = p_win->startx;
+  y = p_win->starty;
+  w = p_win->width;
+  h = p_win->height;
+  if(flag == TRUE)
+    {
+      move( y+0,x ); addstr("  ##        ##  ");
+      move( y+1,x ); addstr("    #      #    ");
+      move( y+2,x ); addstr("  ############  ");
+      move( y+3,x ); addstr(" ###  ####  ### ");
+      move( y+4,x ); addstr("################");
+      move( y+5,x ); addstr("# ############ #");
+      move( y+6,x ); addstr("# #          # #");
+      move( y+7,x ); addstr("   ##      ##   ");
+    }
+  else
+    for(j = y; j <= y + h; ++j)
+      for(i = x; i <= x + w; ++i)
+	mvaddch(j, i, ' ');
+  refresh();
+}
+
+
 
 
 string printMap(){
@@ -34,13 +99,31 @@ string printMap(){
 
 void nuevoUsuario( int ConnectFD){
   //  string menu="\n----------------------------\nMENU\n 1. [Action P] Print list of user on the chat\n 2. [Action L] Login to the chat\n 3. [Action C] Send a msg to an user on the chat\n 4. [Action F] Send File \n 6. [Action E] End chat or logout from chat \n----------------------------";
+
   string mensaje=write_protocol_R("Bienvenido al juego\n-------------------");
+  
+  WIN win;
+  //    WIN win2;
+  jugar(win);
 
   int sizef; 
   char tipo=getTypeProtocol(ConnectFD,sizef);
   string nick=read_protocol_L(ConnectFD,sizef);
   Users[nick]=ConnectFD;
 
+  int ch;
+
+     initscr();                      /* Start curses mode            */
+     start_color();                  /* Start the color functionality */
+     cbreak();                       /* Line buffering disabled, Pass on*/
+     keypad(stdscr, TRUE);           /* I need that nifty F1         */
+     noecho();
+     
+     init_win_params(&win);
+  //    print_win_params(&win);
+  //    attron(COLOR_PAIR(1));
+  //    attroff(COLOR_PAIR(1));
+  create_box(&win, TRUE);
   my_writeSimple(ConnectFD,mensaje);
   while(true){ 
     //my_writeSimple(ConnectFD,menu);
@@ -48,8 +131,33 @@ void nuevoUsuario( int ConnectFD){
    if (tipo == 'R'){
      string msj;
      string nv= read_protocol_RGame(ConnectFD,sizef);
-     nv=write_protocol_R(nick+" dice: "+nv);
-     sendAllMap(Users,nv);
+     //nv=write_protocol_R(nick+" dice: "+nv);
+     //sendAllMap(Users,nv);
+       //para ver al inicio
+     int ch=atoi(nv.c_str());
+     switch(ch)
+	{case KEY_LEFT:
+	    create_box(&win, FALSE);
+	    --win.startx;
+	    create_box(&win, TRUE);
+	    break;
+	case KEY_RIGHT:
+	  create_box(&win, FALSE);
+	  ++win.startx;
+	  create_box(&win, TRUE);
+	  break;
+	case KEY_UP:
+	  create_box(&win, FALSE);
+	  --win.starty;
+	  create_box(&win, TRUE);
+	  break;
+	case KEY_DOWN:
+	  create_box(&win, FALSE);
+	  ++win.starty;
+	  create_box(&win, TRUE);
+	  break;
+	}
+      
    }
    /*   else if (tipo == 'E'){
      //shutdown(ConnectFD, SHUT_RDWR);
